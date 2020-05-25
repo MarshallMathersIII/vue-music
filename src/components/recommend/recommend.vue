b987ee1843b71a5d150000fd30f4425d52bcf1a5
eminem <ghy1060627080@163.com>
2020-05-25 17:41:25 +0800

    歌单列表懒加载

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,6 +1,20 @@
 <template>
   <div>
     <swiper :list="recommends"></swiper>
+    <div class="recommend-list">
+      <div class="list-title">热门歌单推荐</div>
+      <ul>
+        <li class="item" v-for="(item, index) of discList" :key="index">
+          <div class="icon">
+            <img v-lazy="item.imgurl" width="60" height="60" />
+          </div>
+          <div class="text">
+            <h2 class="name" v-html="item.creator.name"></h2>
+            <p class="desc" v-html="item.dissname"></p>
+          </div>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -12,7 +26,8 @@
 export default {
     data() {
         return {
-            recommends:[]
+            recommends:[],
+            discList:[]
         }
     },
     components:{
@@ -35,6 +50,7 @@ export default {
         getDiscList().then((res)=>{
           if (res.code === ERR_OK) {
             console.log(res.data.list)
+            this.discList=res.data.list
           }
         })
       }
@@ -42,4 +58,37 @@ export default {
 }
 </script>
 
-<style lang="stylus"></style>
+<style scoped lang="stylus" rel="stylesheet/stylus">
+@import "~common/stylus/variable"
+    .recommend-list
+        .list-title
+            height 65px
+            line-height 65px
+            text-align center
+            font-size $font-size-medium
+            color $color-theme
+        .item
+            box-sizing border-box
+            color $color-theme
+            display flex
+            padding 0 20px 20px 20px
+            align-items center
+            .icon
+                flex 0 0 60px
+                width 60px
+                height 60px
+            .text
+                margin-left 20px
+                display flex
+                flex-direction column
+                justify-content center
+                flex 1
+                line-height 20px
+                overflow hidden
+                font-size $font-size-medium
+                .name
+                    margin-bottom: 10px
+                    color $color-text
+                .desc
+                    color $color-text-d
+</style>
--- src/main.js
+++ src/main.js
@@ -10,10 +10,18 @@ import 'common/stylus/index.styl'
 import VueAwesomeSwiper from 'vue-awesome-swiper'
 //swipper必须
 require('swiper/dist/css/swiper.css')
+//懒加载
+import VueLazyload from 'vue-lazyload'
 
 Vue.config.productionTip = false
 fastlick.attach(document.body)
 Vue.use(VueAwesomeSwiper)
+Vue.use(VueLazyload, {
+  // preLoad: 1.3, //预加载的宽高比
+  // error: 'dist/error.png', //图片加载失败时使用的图片源
+  // attempt: 1 //尝试加载次数
+  loading: require('common/image/default.png') //图片加载的路径
+})
 
 new Vue({
   render: h => h(App),
