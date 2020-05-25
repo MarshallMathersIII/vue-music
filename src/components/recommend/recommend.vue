74b53cfbc1ffa5ab38e0e59562a9e67577149420
eminem <ghy1060627080@163.com>
2020-05-25 14:45:11 +0800

    代理请求歌单接口

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -7,7 +7,7 @@
 <script type="text/ecmascript-6">
   import Swiper from 'components/recommend/swiper'
 
-  import {getRecommend} from 'api/recommend'
+  import {getRecommend,getDiscList} from 'api/recommend'
   import {ERR_OK} from 'api/config'
 export default {
     data() {
@@ -19,8 +19,8 @@ export default {
         Swiper
     },
     created(){
-        this._getRecommend()
-
+        this._getRecommend(),
+        this._getDiscList()
     },
     methods: {
      _getRecommend() {
@@ -28,6 +28,13 @@ export default {
           if (res.code === ERR_OK) {
             console.log(res.data.slider)
             this.recommends = res.data.slider
+          }
+        })
+      },
+      _getDiscList(){
+        getDiscList().then((res)=>{
+          if (res.code === ERR_OK) {
+            console.log(res.data.list)
           }
         })
       }
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,8 @@
+const express = require('express')
+const app = express()
+var axios = require('axios')
+const apiRoutes = express.Router() //后端路由
+app.use('/api', apiRoutes)
 module.exports = {
   chainWebpack: config => {
     // 设置别名
@@ -6,5 +11,27 @@ module.exports = {
       .set('components', '@/components')
       .set('common', '@/common')
       .set('api', '@/api')
+  },
+  devServer: {
+    before(app) {
+      // 后端代理
+      app.get('/api/getDiscList', function(req, res) {
+        var url = 'https://c.y.qq.com/splcloud/fcgi-bin/fcg_get_diss_by_tag.fcg'
+        axios
+          .get(url, {
+            headers: {
+              referer: 'https://c.y.qq.com/',
+              host: 'c.y.qq.com'
+            },
+            params: req.query //前端传过来的数据
+          })
+          .then(response => {
+            res.json(response.data)
+          })
+          .catch(e => {
+            console.log(e)
+          })
+      })
+    }
   }
 }
