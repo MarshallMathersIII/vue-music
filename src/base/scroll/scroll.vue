12ec6711fcefd91522b2548b801c1b949f74b9cf
eminem <ghy1060627080@163.com>
2020-05-26 14:53:36 +0800

    better-scroll歌单列表滚动

---
+++ src/base/scroll/scroll.vue
@@ -0,0 +1,82 @@
+<template>
+  <div ref="wrapper">
+    <slot></slot>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+import BScroll from 'better-scroll'
+
+export default {
+  props: {
+    probeType: {
+      type: Number,
+      default: 1
+    },
+    click: {
+      type: Boolean,
+      default: true
+    },
+    listenScroll: {
+      type: Boolean,
+      default: false
+    },
+    data: {
+      type: Array,
+      default: null
+    },
+    pullup: {
+      type: Boolean,
+      default: false
+    },
+    beforeScroll: {
+      type: Boolean,
+      default: false
+    },
+    refreshDelay: {
+      type: Number,
+      default: 20
+    }
+  },
+  mounted() {
+    setTimeout(() => {
+      this._initScroll()
+    }, 20)
+  },
+  methods: {
+    _initScroll() {
+      if (!this.$refs.wrapper) {
+        return
+      }
+      console.log("111111initScroll")
+      this.scroll = new BScroll(this.$refs.wrapper, {
+        probeType: this.probeType,
+        click: this.click,
+
+
+      }),
+    	console.log(this.scroll)
+
+    },
+    disable() {
+      this.scroll && this.scroll.disable()
+    },
+    enable() {
+      this.scroll && this.scroll.enable()
+    },
+    refresh() {
+      this.scroll && this.scroll.refresh()
+    },
+  },
+//监听调用组件数据实时刷新
+  watch: {
+    data() {
+      setTimeout(() => {
+        this.refresh()
+      }, this.refreshDelay)
+    }
+  }
+}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus"></style>
--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,30 +1,39 @@
 <template>
-  <div>
-    <swiper :list="recommends"></swiper>
-    <div class="recommend-list">
-      <div class="list-title">热门歌单推荐</div>
-      <ul>
-        <li class="item" v-for="(item, index) of discList" :key="index">
-          <div class="icon">
-            <img v-lazy="item.imgurl" width="60" height="60" />
-          </div>
-          <div class="text">
-            <h2 class="name" v-html="item.creator.name"></h2>
-            <p class="desc" v-html="item.dissname"></p>
-          </div>
-        </li>
-      </ul>
-    </div>
-    <div class="loading-container" v-show="!discList.length">
-      <loading></loading>
-    </div>
+  <div class="recommend" ref="recommend">
+    <scroll ref="scroll" class="recommend-content" :data="discList">
+      <div>
+        <swiper :list="recommends"></swiper>
+        <div class="recommend-list">
+          <div class="list-title">热门歌单推荐</div>
+          <ul>
+            <li class="item" v-for="(item, index) of discList" :key="index">
+              <div class="icon">
+                <img
+                  @load="loadImage"
+                  v-lazy="item.imgurl"
+                  width="60"
+                  height="60"
+                />
+              </div>
+              <div class="text">
+                <h2 class="name" v-html="item.creator.name"></h2>
+                <p class="desc" v-html="item.dissname"></p>
+              </div>
+            </li>
+          </ul>
+        </div>
+        <div class="loading-container" v-show="!discList.length">
+          <loading></loading>
+        </div>
+      </div>
+    </scroll>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
   import Swiper from 'components/recommend/swiper'
   import Loading from 'base/loading/loading'
-
+  import Scroll from 'base/scroll/scroll'
 
   import {getRecommend,getDiscList} from 'api/recommend'
   import {ERR_OK} from 'api/config'
@@ -37,11 +46,12 @@ export default {
     },
     components:{
         Swiper,
-        Loading
+        Loading,
+        Scroll
     },
     created(){
         this._getRecommend(),
-                    this._getDiscList()
+        this._getDiscList()
     },
     methods: {
      _getRecommend() {
@@ -59,6 +69,14 @@ export default {
             this.discList=res.data.list
           }
         })
+      },
+      loadImage() {
+        if (!this.checkloaded) {
+          this.checkloaded = true
+          setTimeout(() => {
+            this.$refs.scroll.refresh()
+          }, 20)
+        }
       }
     }
 }
@@ -66,40 +84,48 @@ export default {
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
 @import "~common/stylus/variable"
-    .recommend-list
-        .list-title
-            height 65px
-            line-height 65px
-            text-align center
-            font-size $font-size-medium
-            color $color-theme
-        .item
-            box-sizing border-box
-            color $color-theme
-            display flex
-            padding 0 20px 20px 20px
-            align-items center
-            .icon
-                flex 0 0 60px
-                width 60px
-                height 60px
-            .text
-                margin-left 20px
-                display flex
-                flex-direction column
-                justify-content center
-                flex 1
-                line-height 20px
-                overflow hidden
+.recommend
+    position: fixed
+    width: 100%
+    top: 88px
+    bottom: 0
+    .recommend-content
+        height: 100%
+        overflow: hidden
+        .recommend-list
+            .list-title
+                height 65px
+                line-height 65px
+                text-align center
                 font-size $font-size-medium
-                .name
-                    margin-bottom: 10px
-                    color $color-text
-                .desc
-                    color $color-text-d
-        .loading-container
-            position: absolute
-            width: 100%
-            top: 50%
-            transform: translateY(-50%)
+                color $color-theme
+            .item
+                box-sizing border-box
+                color $color-theme
+                display flex
+                padding 0 20px 20px 20px
+                align-items center
+                .icon
+                    flex 0 0 60px
+                    width 60px
+                    height 60px
+                .text
+                    margin-left 20px
+                    display flex
+                    flex-direction column
+                    justify-content center
+                    flex 1
+                    line-height 20px
+                    overflow hidden
+                    font-size $font-size-medium
+                    .name
+                        margin-bottom: 10px
+                        color $color-text
+                    .desc
+                        color $color-text-d
+            .loading-container
+                position: absolute
+                width: 100%
+                top: 50%
+                transform: translateY(-50%)
 </style>
