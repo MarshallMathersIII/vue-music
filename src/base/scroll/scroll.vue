039e47da56ca033114a8d371008709672952658d
eminem <ghy1060627080@163.com>
2020-05-29 16:55:57 +0800

    歌手列表

--- src/base/scroll/scroll.vue
+++ src/base/scroll/scroll.vue
@@ -7,6 +7,9 @@
 <script type="text/ecmascript-6">
 import BScroll from 'better-scroll'
 
+const DIRECTION_H = 'horizontal'
+const DIRECTION_V = 'vertical'
+
 export default {
   props: {
     probeType: {
@@ -36,6 +39,10 @@ export default {
     refreshDelay: {
       type: Number,
       default: 20
+    },
+    direction: {
+      type: String,
+      default: DIRECTION_V
     }
   },
   mounted() {
@@ -48,15 +55,31 @@ export default {
       if (!this.$refs.wrapper) {
         return
       }
-      console.log("111111initScroll")
       this.scroll = new BScroll(this.$refs.wrapper, {
         probeType: this.probeType,
         click: this.click,
+        eventPassthrough: this.direction === DIRECTION_V ? DIRECTION_H : DIRECTION_V
+      })
 
+      if (this.listenScroll) {
+        this.scroll.on('scroll', (pos) => {
+          this.$emit('scroll', pos)
+        })
+      }
 
-      }),
-    	console.log(this.scroll)
+      if (this.pullup) {
+        this.scroll.on('scrollEnd', () => {
+          if (this.scroll.y <= (this.scroll.maxScrollY + 50)) {
+            this.$emit('scrollToEnd')
+          }
+        })
+      }
 
+      if (this.beforeScroll) {
+        this.scroll.on('beforeScrollStart', () => {
+          this.$emit('beforeScroll')
+        })
+      }
     },
     disable() {
       this.scroll && this.scroll.disable()
@@ -67,8 +90,13 @@ export default {
     refresh() {
       this.scroll && this.scroll.refresh()
     },
+    scrollTo() {
+      this.scroll && this.scroll.scrollTo.apply(this.scroll, arguments)
+    },
+    scrollToElement() {
+      this.scroll && this.scroll.scrollToElement.apply(this.scroll, arguments)
+    }
   },
-//监听调用组件数据实时刷新
   watch: {
     data() {
       setTimeout(() => {
--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,91 +1,100 @@
 <template>
-  <div class="singer">
-    <list-view :data="singers"></list-view>
+  <div class="singer" ref="singer">
+    <list-view @select="selectSinger" :data="singers" ref="list"></list-view>
+    <router-view></router-view>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
-    import ListView from 'base/listview/listview'
-    import {getSingerList} from 'api/singer'
-    import {ERR_OK} from 'api/config'
-    import Singer from 'common/js/singer'
+import ListView from 'base/listview/listview'
+import {getSingerList} from 'api/singer'
+import {ERR_OK} from 'api/config'
+import Singer from 'common/js/singer'
 
-    const HOT_SINGER_LEN = 10
-    const HOT_NAME = '热门'
+const HOT_SINGER_LEN = 10
+const HOT_NAME = '热门'
 
 export default {
-     data() {
-        return {
-            singers:[]
+  data() {
+    return {
+      singers: []
+    }
+  },
+  created() {
+    this._getSingerList()
+  },
+  methods: {
+    handlePlaylist(playlist) {
+      const bottom = playlist.length > 0 ? '60px' : ''
+      this.$refs.singer.style.bottom = bottom
+      this.$refs.list.refresh()
+    },
+    selectSinger(singer) {
+      this.$router.push({
+        path: `/singer/${singer.id}`
+      })
+      this.setSinger(singer)
+    },
+    _getSingerList() {
+      getSingerList().then((res) => {
+        if (res.code === ERR_OK) {
+          this.singers = this._normalizeSinger(res.data.list)
         }
+      })
     },
-    components:{
-        ListView
-    },
-    created(){
-        this._getSingerList()
-    },
-    methods: {
-     _getSingerList() {
-        getSingerList().then((res) => {
-          if (res.code === ERR_OK) {
-              this.singers = this._normalizeSinger(res.data.list)
-            console.log(this.singers)
-          }
-        })
-      },
-      _normalizeSinger(list) {
-        let map = {
-          hot: {
-            title: HOT_NAME,
-            items: []
-          }
+    _normalizeSinger(list) {
+      let map = {
+        hot: {
+          title: HOT_NAME,
+          items: []
         }
-        list.forEach((item, index) => {
-          if (index < HOT_SINGER_LEN) {
-            map.hot.items.push(new Singer({
-              name: item.Fsinger_name,
-              id: item.Fsinger_mid
-            }))
-          }
-          const key = item.Findex
-          if (!map[key]) {
-            map[key] = {
-              title: key,
-              items: []
-            }
-          }
-          map[key].items.push(new Singer({
+      }
+      list.forEach((item, index) => {
+        if (index < HOT_SINGER_LEN) {
+          map.hot.items.push(new Singer({
             name: item.Fsinger_name,
             id: item.Fsinger_mid
           }))
-        })
-        // 为了得到有序列表，我们需要处理 map
-        let ret = []
-        let hot = []
-        for (let key in map) {
-          let val = map[key]
-          if (val.title.match(/[a-zA-Z]/)) {
-            ret.push(val)
-          } else if (val.title === HOT_NAME) {
-            hot.push(val)
+        }
+        const key = item.Findex
+        if (!map[key]) {
+          map[key] = {
+            title: key,
+            items: []
           }
         }
-        ret.sort((a, b) => {
-          return a.title.charCodeAt(0) - b.title.charCodeAt(0)
-        })
-        return hot.concat(ret)
+        map[key].items.push(new Singer({
+          name: item.Fsinger_name,
+          id: item.Fsinger_mid
+        }))
+      })
+      // 为了得到有序列表，我们需要处理 map
+      let ret = []
+      let hot = []
+      for (let key in map) {
+        let val = map[key]
+        if (val.title.match(/[a-zA-Z]/)) {
+          ret.push(val)
+        } else if (val.title === HOT_NAME) {
+          hot.push(val)
+        }
       }
-}
+      ret.sort((a, b) => {
+        return a.title.charCodeAt(0) - b.title.charCodeAt(0)
+      })
+      return hot.concat(ret)
+    }
+  },
+  components: {
+    ListView
+  }
 }
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
-@import "~common/stylus/variable"
-//better-scroll父容器高度需固定
 .singer
-    position: fixed
-    width: 100%
-    top: 88px
-    bottom: 0
+  position: fixed
+  top: 88px
+  bottom: 0
+  width: 100%
 </style>
