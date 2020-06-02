2fbcb32212da46c0aec1a2b28aa5142b37819a27
eminem <ghy1060627080@163.com>
2020-06-02 17:59:51 +0800

    music-list组件

--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -1,6 +1,6 @@
 <template>
   <transition name="slide">
-    <div class="singer-detail"></div>
+    <music-list :title="title" :bg-image="bgImage" :songs="songs"></music-list>
   </transition>
 </template>
 
@@ -10,11 +10,7 @@ import {getSingerDetail} from 'api/singer'
 import {ERR_OK} from 'api/config'
 import {mapGetters} from 'vuex'
 import {createSong } from 'common/js/song'
-
-
-
-
-
+import MusicList from 'components/music-list/music-list'
 export default {
   data() {
     return {
@@ -25,7 +21,13 @@ export default {
     ...mapGetters([
       //语法糖等同于 console.log(this.$store.getters.singer)
       "singer"
-    ])
+    ]),
+    title(){
+     return this.singer.name
+    },
+    bgImage(){
+      return this.singer.avatar
+    }
   },
   created() {
     this._getDetail()
@@ -56,22 +58,15 @@ export default {
         return ret
       }
   },
+  components:{
+    MusicList
+  }
 
 }
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
 @import "~common/stylus/variable"
-
-.singer-detail
-    position fixed
-    z-index 100
-    top 0
-    left 0
-    right 0
-    bottom 0
-    background $color-background
-
 .slide-enter-active, .slide-leave-active
   transition: all 0.9s
 
