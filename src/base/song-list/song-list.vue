830f34e5887f10575a09708896284296d1b489b6
eminem <ghy1060627080@163.com>
2020-06-03 16:46:43 +0800

    歌手详情页music-list

---
+++ src/base/song-list/song-list.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="song-list">
+    <ul>
+      <li class="item" v-for="(song, index) in songs" :key="index">
+        <div class="content">
+          <h2 class="name">{{ song.name }}</h2>
+          <p class="desc">{{ getDesc(song) }}</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+  props: {
+    songs: {
+      type: Array,
+      // eslint-disable-next-line vue/require-valid-default-prop
+      default: []
+    }
+  },
+  methods: {
+    getDesc(song) {
+      //es6字符串模版占位，等价于song.singer+"."+song.album
+      return `${song.singer}·${song.album}`
+    }
+  }
+}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+@import "~common/stylus/variable"
+@import "~common/stylus/mixin"
+
+.song-list
+  .item
+    display: flex
+    align-items: center
+    box-sizing: border-box
+    height: 64px
+    font-size: $font-size-medium
+    .content
+      flex: 1
+      line-height: 20px
+      overflow: hidden
+      .name
+        no-wrap()
+        color: $color-text
+      .desc
+        no-wrap()
+        margin-top: 4px
+        color: $color-text-d
+</style>
